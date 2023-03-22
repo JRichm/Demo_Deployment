8e54b17d27adf31ae7836c6c7b4d6905d584673c
JRichm <jblackenmusic@gmail.com>
2023-03-22 12:27:53 -0600

    sever now serves html, css, and script files

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const cores = require('cores');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +'/public/index.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/styles.css');
+});
+
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/main.js');
+});
 
 app.listen(5150, () => console.log(`app is up on 5150`));
