9c4c335bcdf0d7ec7844c71126e633dd50171faa
JRichm <jblackenmusic@gmail.com>
2023-03-22 15:14:05 -0600

    updated colors and cat name

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Simba')
 
 btn.addEventListener('click', clickHandler)
