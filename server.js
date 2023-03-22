243afdd68d16936c1fcaf63e84833592f915cd0c
JRichm <jblackenmusic@gmail.com>
2023-03-22 15:09:14 -0600

    removed cors

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname +'/public/index.html');
